3054d57b30d6cf593d70525c9706ab6dbdba5ca2
carloslima1991 <carloslima.ptit@gmail.com>
2024-08-24 22:49:15 +0100

    change route

--- dist/main.js
+++ dist/main.js
@@ -14,38 +14,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const swagger_1 = __importDefault(require("./docs/swagger"));
-const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const compression_1 = __importDefault(require("compression"));
 const path_1 = __importDefault(require("path"));
+const swagger_1 = __importDefault(require("./docs/swagger"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const homeRoutes_1 = __importDefault(require("./routes/homeRoutes"));
+// Load environment variables
 dotenv_1.default.config();
+// App configuration
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4000;
-const MONGO = process.env.MONGO_URI || "";
+const MONGO_URI = process.env.MONGO_URI || "";
+// Middleware setup
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
-app.use(express_1.default.static("static"));
-app.use(body_parser_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
 app.use((0, express_fileupload_1.default)());
 app.use((0, compression_1.default)());
-// Serve static files from the 'views' directory
+app.use(body_parser_1.default.json());
+// Static file serving
+app.use(express_1.default.static("static"));
 app.use(express_1.default.static(path_1.default.join(__dirname, "views")));
-app.get("/", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "views", "index.html"));
-});
-//import user and productss
+// Use the home route
+app.use("/", homeRoutes_1.default);
+// API Routes
 app.use("/api", productRoutes_1.default);
 app.use("/auth", userRoutes_1.default);
 const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         mongoose_1.default.set("strictQuery", true);
-        yield mongoose_1.default.connect(String(MONGO));
+        yield mongoose_1.default.connect(String(MONGO_URI));
         console.log("sucessely connected to database");
         app.listen(PORT, () => {
             console.log(`server is ok at http://localhost:${PORT}`);
