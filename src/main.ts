9b220caf18a8efa6fa457ebbbfd7e2219eaa511d
carloslima1991 <carloslima.ptit@gmail.com>
2024-08-24 22:06:39 +0100

    org maim.ts

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,48 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import dotenv from "dotenv";
-import productRoutes from "./routes/productRoutes";
+import mongoose from "mongoose";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import bodyParser from "body-parser";
-import mongoose, {Connection} from "mongoose";
-import SetupSwagger from "./docs/swagger";
-import userRoutes from "./routes/userRoutes";
 import compression from "compression";
 import path from "path";
-import {Request, Response} from "express";
+import SetupSwagger from "./docs/swagger";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
+// Load environment variables
 dotenv.config();
+
+// App configuration
 const app = express();
 const PORT = process.env.PORT || 4000;
-const MONGO = process.env.MONGO_URI || "";
+const MONGO_URI = process.env.MONGO_URI || "";
 
+// Middleware setup
 app.use(express.json());
-app.use(cors());
-app.use(express.static("static"));
-app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(fileUpload());
 app.use(compression());
+app.use(bodyParser.json());
 
-// Serve static files from the 'views' directory
+// Static file serving
+app.use(express.static("static"));
 app.use(express.static(path.join(__dirname, "views")));
 
+// Routes setup
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-//import user and productss
+// API Routes
 app.use("/api", productRoutes);
 app.use("/auth", userRoutes);
 
 const startApp = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(String(MONGO));
+    await mongoose.connect(String(MONGO_URI));
     console.log("sucessely connected to database");
     app.listen(PORT, () => {
       console.log(`server is ok at http://localhost:${PORT}`);
